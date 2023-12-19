4ab9b9e25b541145f1a409c4d9a0bd817d7d6bfa
OrangeAppleMax <eceernayub@gmail.com>
2023-12-19 11:16:05 -0800

    Fixed tiny bugs

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,7 +1,38 @@
+"use client";
+
+import { MessageSquare } from "lucide-react";
+import { Card } from "@/components/ui/card";
+
+const tools = [
+  {
+    label: "Conversation",
+    icon: MessageSquare,
+    color: "text-green-500",
+    bgColor: "text-green-500/10",
+    href: "/conversation"
+  }
+]
+
 export default function DashboardHomepage() {
   return (
     <div>
-      <p>Dashboard Page (Protected)</p>
+      <div className = "mb-8 space-y-4">
+        <h2 className = "text-2xl md: text-4xl font-bold text-center">
+          Play with AI
+        </h2>
+        <p className = "text-muted-foreground font-light text-sm md: text-lg text-center">
+          Explore the various experiences provided to you in this playground
+        </p>
+        </div>
+        <div className = "px-4 md: px-20 lg: px-32 space-y-4">
+        {tools.map((tool) => (
+          <Card key = {tool.href}
+          className = "p-4 border-black/5"
+          >
+
+          </Card>
+        ))}
+        </div>
     </div>
   )
 }
